50f9933f4eb670421eb01b866109ba0a53b5d409
Jakobi Haskell <jakobi_haskell@brown.edeu>
2022-02-03 20:41:34 -0500

    fixes s3 url posting issue

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import { generateUploadURL } from "./s3.js";
+import cors from "cors";
 //const express = require('express');
 //const s3 = require('s3')
 const app = express();
-
 app.use(express.static("front"));
-
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
 app.get("/s3", async (req, res) => {
   const url = await generateUploadURL();
-  res.send({ url });
+  console.log(url)
+  res.send({url});
 });
 
 app.listen(8454, () => console.log(`server listening at 8454`));
