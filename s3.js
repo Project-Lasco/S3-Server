8e73d60efa02f8d0fab0207028e4ea01d4f9dd29
Jakobi Haskell <jakobi_haskell@brown.edeu>
2022-02-03 22:25:21 -0500

    adds nodemon, uplaod of art works

--- s3.js
+++ s3.js
@@ -18,15 +18,26 @@ const s3 = new aws.S3({
   signatureVersion: "v4",
 });
 
-export async function generateUploadURL() {
+export async function generateUploadURL(folder) {
   const rawBytes = await randomBytes(16);
   const imageName = rawBytes.toString("hex");
-
-  const params = {
-    Bucket: bucketName,
-    Key: imageName,
-    Expires: 60,
-  };
+  var params = {};
+  if (folder) {
+    params = {
+      Bucket: bucketName,
+      Key: folder + imageName,
+      Expires: 60,
+    };
+  }
+  else {
+    params = {
+      Bucket: bucketName,
+      Key: imageName,
+      Expires: 60,
+    };
+  }
+ 
+  
 
   const uploadURL = await s3.getSignedUrlPromise("putObject", params);
   return uploadURL;
--- server.js
+++ server.js
@@ -8,10 +8,22 @@ app.use(express.static("front"));
 app.use(cors({
   origin: "http://localhost:3000"
 }))
+// uploads to profile-pics folder
 app.get("/s3", async (req, res) => {
-  const url = await generateUploadURL();
+  const url = await generateUploadURL('profile-pics/');
   console.log(url)
   res.send({url});
 });
+// uploads to artworks folder
+// TODO: think abt changing design of uploads
+app.get("/art", async (req, res) => {
+  const url = await generateUploadURL('user-artwork/');
+  console.log(url)
+  res.send({url});
+});
+
+// app.get("/art", async (req, res) => {
+//   // generates upload url, and sends it back
+// })
 
 app.listen(8454, () => console.log(`server listening at 8454`));
